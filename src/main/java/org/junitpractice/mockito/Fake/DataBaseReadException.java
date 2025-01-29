435e4baeb493335791aedfb69e8455a9decfe42e
anmol srivastava <anmol.srivastava@arrise.com>
2025-01-29 18:02:31 +0530

    29 Jan Implementing mockito for static method and exception throwing void/non void methods

---
+++ src/main/java/org/junitpractice/mockito/Fake/DataBaseReadException.java
@@ -0,0 +1,7 @@
+package org.junitpractice.mockito.Fake;
+
+public class DataBaseReadException extends RuntimeException {
+    public DataBaseReadException(String message) {
+        super(message);
+    }
+}
